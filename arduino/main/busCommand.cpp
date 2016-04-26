90182aeda498a6cb1b578ffa54772c4e1c19f201
Macarescu Sebastian <sebastian.macarescu@assist.ro>
2016-04-26 17:19:30 +0300

    blue working

--- arduino/main/busCommand.cpp
+++ arduino/main/busCommand.cpp
@@ -4,18 +4,18 @@
  * Constructor makes sure some things are set.
  */
 busCommand::busCommand()
-  : commandList(NULL),
-    commandCount(0),
-    defaultHandler(NULL),
-    transmissionEnd(';'),         // default terminator for transmission
-    commandDelim(','),
-    argDelim(' '),
-    last(buffer),
-    isI2C(false),
-    isFinished(false),
-    readyToParse(false)
+	: commandList(NULL),
+		commandCount(0),
+		defaultHandler(NULL),
+		transmissionEnd(';'),         // default terminator for transmission
+		commandDelim(','),
+		argDelim(' '),
+		last(buffer),
+		isI2C(false),
+		isFinished(false),
+		readyToParse(false)
 {
-  clearBuffer();
+	clearBuffer();
 }
  
 /**
@@ -24,17 +24,17 @@ busCommand::busCommand()
  * to the handler function to deal with it.
  */
 void busCommand::addCommand(const char *command, void (*function)()) {
-  #ifdef busCommand_DEBUG
-    Serial.print("Adding command (");
-    Serial.print(commandCount);
-    Serial.print("): ");
-    Serial.println(command);
-  #endif
+	#ifdef busCommand_DEBUG
+		Serial.print("Adding command (");
+		Serial.print(commandCount);
+		Serial.print("): ");
+		Serial.println(command);
+	#endif
  
-  commandList = (busCommandCallback *) realloc(commandList, (commandCount + 1) * sizeof(busCommandCallback));
-  strncpy(commandList[commandCount].command, command, busCommand_MAXCOMMANDLENGTH);
-  commandList[commandCount].function = function;
-  commandCount++;
+	commandList = (busCommandCallback *) realloc(commandList, (commandCount + 1) * sizeof(busCommandCallback));
+	strncpy(commandList[commandCount].command, command, busCommand_MAXCOMMANDLENGTH);
+	commandList[commandCount].function = function;
+	commandCount++;
 }
  
 /**
@@ -42,7 +42,7 @@ void busCommand::addCommand(const char *command, void (*function)()) {
  * isn't in the list of commands.
  */
 void busCommand::setDefaultHandler(void (*function)(const char *)) {
-  defaultHandler = function;
+	defaultHandler = function;
 }
  
  
@@ -52,165 +52,165 @@ void busCommand::setDefaultHandler(void (*function)(const char *)) {
  * buffer for a prefix command, and calls handlers setup by addCommand() member
  */
 void busCommand::readBus() {
-   
-  while (isAvailable() > 0) {
-    char inChar = busRead();   // Read single available character, there may be more waiting
-    if(inChar == '\r' || inChar == '\n') continue; //Skip CR and NEWLINE
-    #ifdef busCommand_DEBUG
-      Serial.print(inChar);   // Echo back to serial stream
-    #endif
-    if (inChar == transmissionEnd) {     // Check for the terminator (default ';')
-      readyToParse = true;
-    }
-    if (bufPos < busCommand_BUFFER) {
-      buffer[bufPos++] = inChar;  // Put character into buffer
-      buffer[bufPos] = '\0';      // Null terminate
-    } else {
-      #ifdef busCommand_DEBUG
-        Serial.println("Line buffer is full - increase busCommand_BUFFER");
-      #endif
-    }
-  }
+	 
+	while (isAvailable() > 0) {
+		char inChar = busRead();   // Read single available character, there may be more waiting
+		if(inChar == '\r' || inChar == '\n') continue; //Skip CR and NEWLINE
+		#ifdef busCommand_DEBUG
+			Serial.print(inChar);   // Echo back to serial stream
+		#endif
+		if (inChar == transmissionEnd) {     // Check for the terminator (default ';')
+			readyToParse = true;
+		}
+		if (bufPos < busCommand_BUFFER) {
+			buffer[bufPos++] = inChar;  // Put character into buffer
+			buffer[bufPos] = '\0';      // Null terminate
+		} else {
+			#ifdef busCommand_DEBUG
+				Serial.println("Line buffer is full - increase busCommand_BUFFER");
+			#endif
+		}
+	}
 }
  
 char* busCommand::getCommand(){
  
-  hasArgs = true;
-  if(isFinished) 
-    return NULL;
+	hasArgs = true;
+	if(isFinished) 
+		return NULL;
  
-  while(*last == argDelim || *last == commandDelim){
-    *last = '\0';
-    last++;   //Jump over junk
-  }
-  if(*last == transmissionEnd){
-    clearBuffer();
-    isFinished = true;
-    return NULL;
-  }
+	while(*last == argDelim || *last == commandDelim){
+		*last = '\0';
+		last++;   //Jump over junk
+	}
+	if(*last == transmissionEnd){
+		clearBuffer();
+		isFinished = true;
+		return NULL;
+	}
  
-  char *beg = last;
+	char *beg = last;
  
-  while(*last != commandDelim && *last != argDelim && *last != transmissionEnd ) last++;
+	while(*last != commandDelim && *last != argDelim && *last != transmissionEnd ) last++;
  
-  if(*last == commandDelim || *last == transmissionEnd) 
-    hasArgs = false;
-   
-  if(*last == transmissionEnd){
-    isFinished = true;
-  }
-  *last = '\0'; 
-  last++;
-  return beg;
+	if(*last == commandDelim || *last == transmissionEnd) 
+		hasArgs = false;
+	 
+	if(*last == transmissionEnd){
+		isFinished = true;
+	}
+	*last = '\0'; 
+	last++;
+	return beg;
 }
  
 char* busCommand::getArg(){
-  if(hasArgs == false || isFinished)
-    return NULL;
-  while(*last == commandDelim){
-    *last = '\0';
-    last++;
-  }
+	if(hasArgs == false || isFinished)
+		return NULL;
+	while(*last == commandDelim){
+		*last = '\0';
+		last++;
+	}
  
-  char *beg = last;
+	char *beg = last;
  
-  while(*last != commandDelim && *last != argDelim && *last != transmissionEnd) last++;
+	while(*last != commandDelim && *last != argDelim && *last != transmissionEnd) last++;
  
-  if(*last == commandDelim || *last == transmissionEnd) hasArgs = false;
+	if(*last == commandDelim || *last == transmissionEnd) hasArgs = false;
  
-  if(*last == transmissionEnd)
-    isFinished = true;
+	if(*last == transmissionEnd)
+		isFinished = true;
  
-  *last = '\0'; 
-  last++;
+	*last = '\0'; 
+	last++;
  
-  return beg;
+	return beg;
 }
  
 void busCommand::Parse(){
  
-  #ifdef busCommand_DEBUG
-      Serial.print("Received: ");
-      Serial.println(buffer);
-  #endif
+	#ifdef busCommand_DEBUG
+			Serial.print("Received: ");
+			Serial.println(buffer);
+	#endif
  
-  last = buffer;
-  isFinished = false;
-  char *command = getCommand();   // Search for command 
-  while (command != NULL) {
-    boolean matched = false;
-    for (int i = 0; i < commandCount; i++) {
-      #ifdef busCommand_DEBUG
-        Serial.print("Comparing [");
-        Serial.print(command);
-        Serial.print("] to [");
-        Serial.print(commandList[i].command);
-        Serial.println("]");
-      #endif
+	last = buffer;
+	isFinished = false;
+	char *command = getCommand();   // Search for command 
+	while (command != NULL) {
+		boolean matched = false;
+		for (int i = 0; i < commandCount; i++) {
+			#ifdef busCommand_DEBUG
+				Serial.print("Comparing [");
+				Serial.print(command);
+				Serial.print("] to [");
+				Serial.print(commandList[i].command);
+				Serial.println("]");
+			#endif
  
-      // Compare the found command against the list of known commands for a match
-      if (strncmp(command, commandList[i].command, busCommand_MAXCOMMANDLENGTH) == 0) {
-        #ifdef busCommand_DEBUG
-          Serial.print("Matched Command: ");
-          Serial.println(command);
-        #endif
+			// Compare the found command against the list of known commands for a match
+			if (strncmp(command, commandList[i].command, busCommand_MAXCOMMANDLENGTH) == 0) {
+				#ifdef busCommand_DEBUG
+					Serial.print("Matched Command: ");
+					Serial.println(command);
+				#endif
  
-        // Execute the stored handler function for the command
-        (*commandList[i].function)();
-        matched = true;
-        break;
-      }
-    }
-    if (!matched && (defaultHandler != NULL)) {
-      (*defaultHandler)(command);
-    }
-    command = getCommand();
-  }
-  clearBuffer();
+				// Execute the stored handler function for the command
+				(*commandList[i].function)();
+				matched = true;
+				break;
+			}
+		}
+		if (!matched && (defaultHandler != NULL)) {
+			(*defaultHandler)(command);
+		}
+		command = getCommand();
+	}
+	clearBuffer();
  
-  readyToParse = false;
+	readyToParse = false;
 }
  
 /*
  * Clear the input buffer.
  */
 void busCommand::clearBuffer() {
-  buffer[0] = '\0';
-  bufPos = 0;
+	buffer[0] = '\0';
+	bufPos = 0;
 }
 
 void busCommand::switchToI2C(){
-  isI2C = true;
+	isI2C = true;
 }
  
 void busCommand::switchToSerial(){
-  isI2C = false;
+	isI2C = false;
 }
  
 int busCommand::isAvailable(){
-  if(isI2C)
-    return Wire.available();
-  if(customSerial)
-    return customSerial->available();
-  return Serial.available();
+	if(isI2C)
+		return Wire.available();
+	if(customSerial)
+		return customSerial->available();
+	return Serial.available();
 }
  
 char busCommand::busRead(){
-  if(isI2C)
-    return Wire.read();
-  if(customSerial)
-    return customSerial->read();
-  return Serial.read();
+	if(isI2C)
+		return Wire.read();
+	if(customSerial)
+		return customSerial->read();
+	return Serial.read();
 }
 
 void busCommand::setSoftwareSerial(SoftwareSerial *serial){
-  customSerial = serial;
+	customSerial = serial;
 }
 
 boolean busCommand::isSerial(){
-  return !isI2C;
+	return !isI2C;
 }
  
 boolean busCommand::isReadyToParse(){
-  return readyToParse;
+	return readyToParse;
 }
--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -17,7 +17,7 @@ String BlueSensorData = "sensors";
 bool DEBUG = true;
 bool DEBUG_SENSOR_DATA = true;
 
-//SoftwareSerial blueSerial(6, 5); // RX, TX
+SoftwareSerial blueSerial(2, 3); // RX, TX
 
 int dLeft, dFront, dRight;
 boolean IsAutonomous = false;
@@ -45,180 +45,195 @@ Sonar* mysonar;
 NewPing leftSonar(TRIGGER_LEFT, ECHO_LEFT, Sonar::MAX_DISTANCE);
 NewPing rightSonar(TRIGGER_RIGHT, ECHO_RIGHT, Sonar::MAX_DISTANCE);
 
+void setupBlueSerial() {
+	blueSerial.begin(115200);  // The Bluetooth Mate defaults to 115200bps
+  blueSerial.print("$");  // Print three times individually
+  blueSerial.print("$");
+  blueSerial.print("$");  // Enter command mode
+  delay(100);  // Short delay, wait for the Mate to send back CMD
+  blueSerial.println("U,9600,N");  // Temporarily Change the baudrate to 9600, no parity
+  // 115200 can be too fast at times for NewSoftSerial to relay the data reliably
+  blueSerial.begin(9600);  // Start bluetooth serial at 9600
+}
+
 void setup(){
-  //blueSerial.begin(9600);
-  Serial.begin(9600);
+	setupBlueSerial();
+	Serial.begin(9600);
 
-  //Set commands
-  bCmd.addCommand("s", stopMotors);
-  bCmd.addCommand("steer", steer);
-  bCmd.addCommand("t", traction);
-  bCmd.addCommand("b", brake);
-  bCmd.addCommand("a", autoNom);
-  bCmd.setDefaultHandler(unrecognized);
-  //bCmd.setSoftwareSerial(&blueSerial);
-  
-  // mysonar = new Sonar(oneSensorCycle);
-  // mysonar->addSonar(TRIGGER_LEFT, ECHO_LEFT);
-  // mysonar->addSonar(TRIGGER_RIGHT, ECHO_RIGHT);
-  // mysonar->init();
+	//Set commands
+	bCmd.addCommand("s", stopMotors);
+	bCmd.addCommand("steer", steer);
+	bCmd.addCommand("t", traction);
+	bCmd.addCommand("b", brake);
+	bCmd.addCommand("a", autoNom);
+	bCmd.setDefaultHandler(unrecognized);
+	
+	//Comment this line to receive commands over standard serial
+	bCmd.setSoftwareSerial(&blueSerial);
+	
+	// mysonar = new Sonar(oneSensorCycle);
+	// mysonar->addSonar(TRIGGER_LEFT, ECHO_LEFT);
+	// mysonar->addSonar(TRIGGER_RIGHT, ECHO_RIGHT);
+	// mysonar->init();
 
-  //Init other things
-  steeringMotor = new DCMotor(steeringEn, steeringA, steeringB);
-  tractionMotor = new ServoMotor(tractionPin);
+	//Init other things
+	steeringMotor = new DCMotor(steeringEn, steeringA, steeringB);
+	tractionMotor = new ServoMotor(tractionPin);
 
-  Serial.println("Setup complete");
+	Serial.println("Setup complete");
+	blueSerial.println("Setup complete");
 }
 
 void loop(){
-  bCmd.readBus();
-  if(bCmd.isReadyToParse())
-    bCmd.Parse();
-  //mysonar->checkSonar();
-  //replace delay with static member millis
-  oneSensorCycle();
-  steeringMotor->update();
-  tractionMotor->update();
-  //delay(100);
-  //updateSensorData();
-  //sendSerialData();
-  
-  //Code for memory
-  //Serial.print("freeMemory()=");
-  //Serial.println(freeMemory());
-  //
+
+	bCmd.readBus();
+	if(bCmd.isReadyToParse())
+		bCmd.Parse();
+	//mysonar->checkSonar();
+	//replace delay with static member millis
+	//oneSensorCycle();
+	steeringMotor->update();
+	tractionMotor->update();
+	delay(100);
+	//updateSensorData();
+	//sendSerialData();
+	
+	//Code for memory
+	//Serial.print("freeMemory()=");
+	//Serial.println(freeMemory());
+	//
 }
 
 void autoNom(){
-  if(IsAutonomous) {
-    //Stop motors
-    stopMotors();
-  }
-  else {
-    tractionMotor->setSpeed(42);
-  }
-  IsAutonomous = !IsAutonomous;
+	if(IsAutonomous) {
+		//Stop motors
+		stopMotors();
+	}
+	else {
+			tractionMotor->setSpeed(42);
+	}
+	IsAutonomous = !IsAutonomous;
 }
 
 void oneSensorCycle() { 
-  if(!IsAutonomous) return;
-  // Sensor ping cycle complete, do something with the results.
-  dLeft = leftSonar.ping_median() / US_ROUNDTRIP_CM;
-  dRight = rightSonar.ping_median() / US_ROUNDTRIP_CM;
-  // Serial.print("; LF "); Serial.print(cm[0]);
-  // Serial.print("; F "); Serial.print(cm[1]);
-  // Serial.print("; RF "); Serial.print(cm[2]);
-  // Serial.println("}");
- 	
-  // Speed = 120, dLeft = cm[0], dFront = cm[1], dRight = cm[2];
-  // if(dFront == 0) dFront = MAX_DISTANCE;
-  if(dLeft == 0) dLeft = 39;
-  if(dRight == 0) dRight = 39;
+	if(!IsAutonomous) return;
+	// Sensor ping cycle complete, do something with the results.
+	dLeft = leftSonar.ping_median() / US_ROUNDTRIP_CM;
+	dRight = rightSonar.ping_median() / US_ROUNDTRIP_CM;
+	// Serial.print("; LF "); Serial.print(cm[0]);
+	// Serial.print("; F "); Serial.print(cm[1]);
+	// Serial.print("; RF "); Serial.print(cm[2]);
+	// Serial.println("}");
+	
+	// Speed = 120, dLeft = cm[0], dFront = cm[1], dRight = cm[2];
+	// if(dFront == 0) dFront = MAX_DISTANCE;
+	if(dLeft == 0) dLeft = 39;
+	if(dRight == 0) dRight = 39;
 
-  if(dLeft < 40)
-    steer(-250);
-  else if(dRight < 40)
-    steer(250);
-  else
-    steer(0);
-  
-  Serial.print("Done ");
-  Serial.print(dLeft); Serial.print(" ");
-  Serial.print(dRight); Serial.print(" ");
-  Serial.println("");
+	if(dLeft < 40)
+	steer(-250);
+	else if(dRight < 40)
+	steer(250);
+	else
+	steer(0);
+	
+	Serial.print("Done ");
+	Serial.print(dLeft); Serial.print(" ");
+	Serial.print(dRight); Serial.print(" ");
+	Serial.println("");
 
-  
-  //todo: algorithm here  
-  
+	
+	//todo: algorithm here  
+	
 }
 
 void updateSensorData(){
-  static unsigned long lastUpdate = 0;
-  unsigned long now = millis();
+	static unsigned long lastUpdate = 0;
+	unsigned long now = millis();
 
-  if(now - lastUpdate > DataSampleTime){
-    //Analog data (Temp and steering pos)
-    //tractionMotorTemp = analogRead(MOTOR_TEMP_PIN);
-    //steeringMotorTemp = tractionMotorTemp;
-    //steeringPos = analogRead(STEERING_POS_PIN);
-    //battVoltage = analogRead(BAT_VOLTAGE_PIN);
-    //battVoltage = map(battVoltage, 0, 1024, 0, 10);
-    //debug(String("batt read ") + battVoltage, DEBUG);
-    lastUpdate = now;
-  }
+	if(now - lastUpdate > DataSampleTime){
+		//Analog data (Temp and steering pos)
+		//tractionMotorTemp = analogRead(MOTOR_TEMP_PIN);
+		//steeringMotorTemp = tractionMotorTemp;
+		//steeringPos = analogRead(STEERING_POS_PIN);
+		//battVoltage = analogRead(BAT_VOLTAGE_PIN);
+		//battVoltage = map(battVoltage, 0, 1024, 0, 10);
+		//debug(String("batt read ") + battVoltage, DEBUG);
+		lastUpdate = now;
+	}
 }
 
 void sendSerialData(){
-  static unsigned long lastSendTime = millis();
-  unsigned long now = millis();
+	static unsigned long lastSendTime = millis();
+	unsigned long now = millis();
 
-  if(now - lastSendTime > SerialDataTime){
-    //Send data
-    String toSend = String("");
-    toSend += BlueSensorData;
+	if(now - lastSendTime > SerialDataTime){
+		//Send data
+		String toSend = String("");
+		toSend += BlueSensorData;
 
-    //tractionPWM, steeringPWM, steeringMotorTemp, tractionMotorTemp, steeringPos
-    //motorRPM, carSpeed, distanceTraveled, battVoltage
-    //toSend += String(" ") + tractionServoSignal;
-    //toSend += String(" ") + steeringServoSignal;
-    //toSend += String(" ") + steeringPos;
-    //toSend += String(" ") + distance;
+		//tractionPWM, steeringPWM, steeringMotorTemp, tractionMotorTemp, steeringPos
+		//motorRPM, carSpeed, distanceTraveled, battVoltage
+		//toSend += String(" ") + tractionServoSignal;
+		//toSend += String(" ") + steeringServoSignal;
+		//toSend += String(" ") + steeringPos;
+		//toSend += String(" ") + distance;
 
-    //blueSerial.println(toSend);
-    debug(String("Data to send ") + toSend, DEBUG_SENSOR_DATA);
-    lastSendTime = now;
-  }
+		//blueSerial.println(toSend);
+		debug(String("Data to send ") + toSend, DEBUG_SENSOR_DATA);
+		lastSendTime = now;
+	}
 }
 
 /*---------COMMANDS-----------*/
 
 
 void stopMotors(){
-  steeringMotor->setSpeed(0);
-  tractionMotor->setSpeed(0);
-  debug(String(F("Motors stopped")), DEBUG);
+	steeringMotor->setSpeed(0);
+	tractionMotor->setSpeed(0);
+	debug(String(F("Motors stopped")), DEBUG);
 }
 
 void brake(){
-  steeringMotor->setSpeed(0);
+	steeringMotor->setSpeed(0);
 }
 
 void steer(){
-  char *arg;
-  int Speed = 0;
+	char *arg;
+	int Speed = 0;
 
-  arg = bCmd.getArg();
-  if(arg != NULL) Speed = atoi(arg);
+	arg = bCmd.getArg();
+	if(arg != NULL) Speed = atoi(arg);
 
-  steeringMotor->setSpeed(Speed);   
-   
-  //delay(500);
+	steeringMotor->setSpeed(Speed);   
+	
+	//delay(500);
 
-  //analogWrite(steeringPinE, abs(Speed));
-  debug(String(F("Steering signal set to ")) + Speed, DEBUG); 
+	//analogWrite(steeringPinE, abs(Speed));
+	debug(String(F("Steering signal set to ")) + Speed, DEBUG); 
 }
 
 void steer(int Speed){
-  steeringMotor->setSpeed(Speed);
+	steeringMotor->setSpeed(Speed);
 }
 
 void traction(){
-  char *arg;
-  int Speed = 0;
-  arg = bCmd.getArg();
-  if(arg != NULL) Speed = atoi(arg);
-  tractionMotor->setSpeed(Speed);
-  debug(String(F("Traction signal set to ")) + Speed, DEBUG); 
+	char *arg;
+	int Speed = 0;
+	arg = bCmd.getArg();
+	if(arg != NULL) Speed = atoi(arg);
+	tractionMotor->setSpeed(Speed);
+	debug(String(F("Traction signal set to ")) + Speed, DEBUG); 
 }
 
 void unrecognized(const char *command) {
-  Serial.println("What?");
+	Serial.println("What?");
 }
 
 /*---------HELPERS-----------*/
 
 
 void debug(String msg, bool debugVar){
-  if(debugVar)
-    Serial.println(msg);
+	if(debugVar)
+	Serial.println(msg);
 }
